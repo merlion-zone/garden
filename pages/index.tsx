36a7cff511e32c6e3a82e9d150dcc932fc63a521
Fantasticist <100333503+fantasticist@users.noreply.github.com>
2022-06-13 19:25:17 +0800

    Update home page (#4)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
-  return <></>
+  const router = useRouter()
+
+  useEffect(() => {
+    void router.replace(`/portfolio`)
+  }, [router])
+
+  return null
 }
 
 export default Home
