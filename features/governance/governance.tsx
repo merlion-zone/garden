0b0d8a8d4570d6394ae1c19ba6eb6ddc83a7a473
Fantastic <100333503+fantasticist@users.noreply.github.com>
2022-06-21 17:16:18 +0800

    Update gov page

--- features/governance/governance.tsx
+++ features/governance/governance.tsx
@@ -18,10 +18,74 @@ import {
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react'
+import {
+  Proposal,
+  ProposalStatus,
+  TextProposal,
+} from 'cosmjs-types/cosmos/gov/v1beta1/gov'
+import { useMemo, useState } from 'react'
 import NextLink from 'next/link'
+import { formatDistanceToNow } from 'date-fns'
+import { useQueryProposals } from '@/hooks/query'
+
+function ProposalItem({ proposal }: { proposal: Proposal }) {
+  const { type, content } = useMemo(() => {
+    switch (proposal.content!.typeUrl) {
+      case '/cosmos.gov.v1beta1.TextProposal':
+        return {
+          type: 'Text Proposal',
+          content: TextProposal.decode(proposal.content!.value),
+        }
+      default:
+        return { type: 'Unknown', content: { title: '', description: '' } } // TODO
+    }
+  }, [proposal])
+
+  const submitTime = useMemo(() => {
+    return formatDistanceToNow(
+      Number(proposal.submitTime!.seconds) * 1000 +
+        Number(proposal.submitTime!.nanos),
+      { addSuffix: true }
+    )
+  }, [proposal.submitTime])
+
+  return (
+    <NextLink href={`/proposal/${proposal.proposalId.toNumber()}`}>
+      <Flex
+        as="a"
+        w="full"
+        py="4"
+        px="4"
+        rounded="lg"
+        bg="bg-surface"
+        justifyContent="space-between"
+        alignItems="center"
+        cursor="pointer"
+      >
+        <Stack>
+          <HStack>
+            <Text fontSize="xs" color="brand">
+              {proposal.proposalId.toNumber()}
+            </Text>
+            <Divider orientation="vertical" h="3" />
+            <Text fontSize="xs">{type}</Text>
+          </HStack>
+          <Text fontSize="lg" fontWeight="medium">
+            {content.title}
+          </Text>
+          <Text fontSize="xs" color="brand">
+            {submitTime}
+          </Text>
+        </Stack>
+        <Button variant="ghost">Vote</Button>
+      </Flex>
+    </NextLink>
+  )
+}
 
 export function Governance() {
-  const proposals: any[] = [] // TODO
+  const [status, setStatus] = useState<ProposalStatus>(0)
+  const { data } = useQueryProposals(status, '', '')
   return (
     <>
       <Container
@@ -68,7 +132,7 @@ export function Governance() {
         </StatGroup>
       </Container>
       <Container maxW="5xl" py={{ base: '4', md: '8' }}>
-        <Tabs variant="with-line">
+        <Tabs variant="with-line" onChange={(v) => setStatus(v)}>
           <TabList>
             <Tab>All</Tab>
             <Tab>Deposit</Tab>
@@ -78,9 +142,9 @@ export function Governance() {
           </TabList>
         </Tabs>
       </Container>
-      <Container maxW="5xl" py={{}}>
+      <Container maxW="5xl" pb={{ base: '4', md: '8' }}>
         <List spacing="4">
-          {proposals.length <= 0 && (
+          {(data?.proposals.length ?? 0) <= 0 && (
             <ListItem
               py="4"
               px="4"
@@ -92,35 +156,9 @@ export function Governance() {
               No proposals.
             </ListItem>
           )}
-          {proposals.map((p, i) => (
-            <ListItem key={i}>
-              <Flex
-                w="full"
-                py="4"
-                px="4"
-                rounded="lg"
-                bg="bg-surface"
-                justifyContent="space-between"
-                alignItems="center"
-                cursor="pointer"
-              >
-                <Stack>
-                  <HStack>
-                    <Text fontSize="xs" color="brand">
-                      100
-                    </Text>
-                    <Divider orientation="vertical" h="3" />
-                    <Text fontSize="xs">Text proposal</Text>
-                  </HStack>
-                  <Text fontSize="lg" fontWeight="medium">
-                    Proposal 001
-                  </Text>
-                  <Text fontSize="xs" color="brand">
-                    7 days ago
-                  </Text>
-                </Stack>
-                <Button variant="ghost">Vote</Button>
-              </Flex>
+          {data?.proposals.map((p, i) => (
+            <ListItem key={p.proposalId.toNumber()}>
+              <ProposalItem proposal={p} />
             </ListItem>
           ))}
         </List>
