48d5466c6fdb40e0e20a5a71ee4834046c6d00be
Fantasticist <100333503+fantasticist@users.noreply.github.com>
2022-06-25 22:17:13 +0800

    Update gov page for support other proposal (#17)

---
+++ pages/governance/ProposalItem.tsx
@@ -0,0 +1,49 @@
+import { Button, Divider, Flex, HStack, Stack, Text } from '@chakra-ui/react'
+import { useMemo } from 'react'
+import NextLink from 'next/link'
+import { formatDistanceToNow } from 'date-fns'
+import { Proposal } from 'cosmjs-types/cosmos/gov/v1beta1/gov'
+import { decodeContent, getTime } from '../proposal/utils'
+
+export function ProposalItem({ proposal }: { proposal: Proposal }) {
+  const content = decodeContent(proposal.content!)
+
+  const submitTime = useMemo(() => {
+    return formatDistanceToNow(getTime(proposal.submitTime!), {
+      addSuffix: true,
+    })
+  }, [proposal.submitTime])
+
+  return (
+    <NextLink href={`/proposal/${proposal.proposalId.toNumber()}`}>
+      <Flex
+        as="a"
+        w="full"
+        py="4"
+        px="4"
+        rounded="lg"
+        bg="bg-surface"
+        justifyContent="space-between"
+        alignItems="center"
+        cursor="pointer"
+      >
+        <Stack>
+          <HStack>
+            <Text fontSize="xs" color="brand">
+              {proposal.proposalId.toNumber()}
+            </Text>
+            <Divider orientation="vertical" h="3" />
+            <Text fontSize="xs">{content.type}</Text>
+          </HStack>
+          <Text fontSize="lg" fontWeight="medium">
+            {content.title}
+          </Text>
+          <Text fontSize="xs" color="brand">
+            {submitTime}
+          </Text>
+        </Stack>
+        <Button variant="ghost">Vote</Button>
+      </Flex>
+    </NextLink>
+  )
+}
