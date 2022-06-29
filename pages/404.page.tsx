3a7100bc6490f3f8fa53a4ccf2d82c4a10b7e112
Deckard <103876178+deckard1982@users.noreply.github.com>
2022-06-29 12:38:56 +0800

    Add 404 page

---
+++ pages/404.page.tsx
@@ -0,0 +1,39 @@
+import { Text, Box, Center, Stack, Button, HStack } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import notFoundPic from '@/public/images/merlion-sticker.png'
+
+export default function NotFound() {
+  const router = useRouter()
+
+  return (
+    <Center h="full">
+      <HStack spacing="16">
+        <Stack spacing="4">
+          <Text fontSize="lg" color="gray.500">
+            404 Not Found.
+          </Text>
+          <Box>
+            <Text>The requested page was not found.</Text>
+            <Text color="gray.500">That’s all we know.</Text>
+          </Box>
+          <HStack>
+            <Button variant="outline" size="xs" onClick={() => router.back()}>
+              Go Back
+            </Button>
+            <Button
+              variant="outline"
+              size="xs"
+              onClick={() => router.push('/portfolio')}
+            >
+              Home
+            </Button>
+          </HStack>
+        </Stack>
+        <Box w="180px" h="180px" filter="grayscale(75%)">
+          <Image src={notFoundPic} alt="Not found" />
+        </Box>
+      </HStack>
+    </Center>
+  )
+}
