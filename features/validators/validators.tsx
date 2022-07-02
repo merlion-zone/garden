793ca2535a7ae2795e3a5df67e473bd89e39b86e
Fantasticist <100333503+fantasticist@users.noreply.github.com>
2022-07-02 22:13:17 +0800

    Add withdraw all rewards (#29)

--- features/validators/validators.tsx
+++ features/validators/validators.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import {
   Alert,
   AlertDescription,
@@ -14,11 +14,31 @@ import {
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react'
-import { BondStatusString } from '@/hooks'
+import {
+  BondStatusString,
+  useAccountAddress,
+  useConnectWallet,
+  useMerlionClient,
+} from '@/hooks'
 import { ValidatorTable } from './table'
+import { useQueryDelegatorValidators, useQueryValidators } from '@/hooks/query'
+import { useToast } from '@/hooks/useToast'
+import { typeUrls } from '@merlionzone/merlionjs/dist'
+import { TransactionToast } from '@/components/TransactionToast'
 
 export function Validators() {
   const [status, setStatus] = useState<BondStatusString>('BOND_STATUS_BONDED')
+
+  const toast = useToast()
+  const merlionClient = useMerlionClient()
+  const { connected, walletType } = useConnectWallet()
+  const address = useAccountAddress()
+  const { data } = useQueryDelegatorValidators(address?.mer())
+  const isMetaMask = useMemo(() => walletType === 'metamask', [walletType])
+  const hasValidators = useMemo(
+    () => data && data.validators.length > 0,
+    [data]
+  )
 
   const onChange = (i: number) => {
     if (i === 0) {
@@ -30,6 +50,38 @@ export function Validators() {
       if (status === 'BOND_STATUS_UNBONDED') return
       setStatus('BOND_STATUS_UNBONDED')
     }
+  }
+
+  const onWithdraw = () => {
+    if (!connected) {
+      toast({
+        title: 'Please connect wallet first',
+        status: 'warning',
+      })
+      return
+    }
+
+    if (isMetaMask || !hasValidators) return
+
+    const msgs = data!.validators.map(({ operatorAddress }) => ({
+      typeUrl: typeUrls.MsgWithdrawDelegatorReward,
+      value: {
+        delegatorAddress: address!.mer(),
+        validatorAddress: operatorAddress,
+      },
+    }))
+
+    const receiptPromise = merlionClient!.signAndBroadcast(address!.mer(), msgs)
+
+    toast({
+      render: ({ onClose }) => (
+        <TransactionToast
+          title="Withdraw all rewards"
+          receiptPromise={receiptPromise}
+          onClose={onClose}
+        />
+      ),
+    })
   }
 
   return (
@@ -54,7 +106,13 @@ export function Validators() {
             </Heading>
             <Text color="muted">Stake Lion and earn rewards</Text>
           </Stack>
-          <Button variant="primary" rounded="full">
+          <Button
+            variant="primary"
+            rounded="full"
+            display={isMetaMask ? 'none' : 'unset'}
+            isDisabled={isMetaMask || !hasValidators}
+            onClick={onWithdraw}
+          >
             Withdraw all rewards
           </Button>
         </Stack>
